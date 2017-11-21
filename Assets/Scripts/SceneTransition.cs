8c5cc423c2bcad41c8262365e8754359a8ca2d5e
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-11-21 20:02:50 +0900

    Add scene transition system

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("CameraPerformance");
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
