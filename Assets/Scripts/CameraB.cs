cacc1d1a578476f1a2a2a4da28fa6426f01c7937
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-11-21 19:08:20 +0900

    Add Camera_B performance

--- Assets/Scripts/CameraB.cs
+++ Assets/Scripts/CameraB.cs
@@ -6,14 +6,45 @@ using DG.Tweening;
 public class CameraB : MonoBehaviour, ICamera
 {
 
+    [SerializeField] private Transform[] paths;
+    [SerializeField] private GameObject unityChan;
+    [SerializeField] private AnimationCurve customEase;
+
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    private Vector3[] cameraPath;
+
+    private Tween tween;
+
+
+    void Awake()
+    {
+        startPosition = gameObject.transform.position;
+        startRotation = gameObject.transform.rotation;
+
+        cameraPath = new Vector3[] { paths[0].position, paths[1].position };
+    }
+
+    void Update()
+    {
+        transform.LookAt(unityChan.transform);
+    }
+
+
     public void MoveStart()
     {
         Debug.Log("Start CameraB");
+        tween = transform.DOPath(cameraPath, 8f, PathType.CatmullRom)
+                         .SetEase(customEase)
+                         .SetLoops(-1);
     }
 
 
     public void Reset()
     {
-        Debug.Log("Reset CameraB");
+        tween.Kill();
+        gameObject.transform.position = startPosition;
+        gameObject.transform.rotation = startRotation;
     }
 }
