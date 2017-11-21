70782a658cb1bf09c66ff84739e327984ea089a3
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-11-21 19:14:17 +0900

    Add Camera_C performance

--- Assets/Scripts/CameraC.cs
+++ Assets/Scripts/CameraC.cs
@@ -5,15 +5,43 @@ using DG.Tweening;
 
 public class CameraC : MonoBehaviour, ICamera
 {
-    
+    [SerializeField] private Transform[] paths;
+    [SerializeField] private GameObject unityChan;
+
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    private Vector3[] cameraPath;
+
+    private Tween tween;
+
+    void Awake()
+    {
+        startPosition = gameObject.transform.position;
+        startRotation = gameObject.transform.rotation;
+
+        cameraPath = new Vector3[] { paths[0].position, paths[1].position, paths[2].position, startPosition };
+    }
+
+    void Update()
+    {
+        transform.LookAt(unityChan.transform);
+    }
+
+
     public void MoveStart()
     {
         Debug.Log("Start CameraC");
+        tween = transform.DOPath(cameraPath, 20f, PathType.Linear)
+                         .SetEase(Ease.Linear)
+                         .SetLoops(-1);
     }
 
 
     public void Reset()
     {
-        Debug.Log("Reset CameraC");
+        tween.Kill();
+        gameObject.transform.position = startPosition;
+        gameObject.transform.rotation = startRotation;
     }
 }
